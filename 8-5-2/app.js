0700bc78f054a6b5a4ea76c4a75e8efd4cf89372
YutongDu <yutong.du@aalto.fi>
2024-05-13 20:50:20 +0300

    add 8

---
+++ 8-5-2/app.js
@@ -0,0 +1,34 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
+
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const app = new Hono();
+
+const data = {
+    todos: [
+        {
+            name: "Write a TODO app",
+            status: false,
+        },
+        {
+            name: "Show a list of TODOs",
+            status: true,
+        },
+        {
+            name: "Move TODOs to a database",
+            status: false,
+        },
+        {
+            name: "Allow marking TODOs done",
+            status: false,
+        }
+    ],
+};
+
+app.get("/", (c) => {
+    return c.html(eta.render("index.eta", data));
+});
+
+export default app;
+
